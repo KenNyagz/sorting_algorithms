5762c114ff7146fcceb40d4149b8b5587f134aec
KenNyagz <kennyagz681@gmail.com>
2023-11-17 21:40:37 +0300

    radix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,11 +1,10 @@
 #include "sort.h"
 
 /**
-  *
-  *
-  *
-  *
-  *
+  *find_max - finds max int from array
+  *@array: array with data to be sorted
+  *@size: length of array
+  *Return: maximum int found
   */
 
 int find_max(int *array, int size)
@@ -21,12 +20,9 @@ int find_max(int *array, int size)
 }
 
 /**
-  *
-  *
-  *
-  *
-  *
-  *
+  *radix_sort - sorts an array of integers
+  *@array: the array
+  *@size: length of array
   *
   */
 
@@ -36,14 +32,12 @@ void radix_sort(int *array, size_t size)
 	int *count, *output_array;
 
 	if (array == NULL || size < 2)
-	return;
+		return;
 
 	output_array = malloc(sizeof(int) * size);
 	if (output_array == NULL)
-	return;
-
+		return;
 	k = find_max(array, size);
-
 	count = malloc(sizeof(int) * (k + 1));
 	if (count == NULL)
 	{
@@ -51,32 +45,27 @@ void radix_sort(int *array, size_t size)
 		return;
 	}
 	for (pos = 1; (k / pos) > 0; pos *= 10)
-    {
-        /*Initialize all elements with zero*/
-        for (i = 0; i < max_digit; i++)
-            count[i] = 0;
+	{
+		/*Initialize all elements with zero*/
+		for (i = 0; i < max_digit; i++)
+			count[i] = 0;
+		/*Get the count of times that integers appear in array - frequency*/
+		for (i = 0; i < size; i++)
+			++count[(array[i] / pos) % 10];
+		/*Perform a cumulative addition on the integers from left to right*/
+		for (i = 1; i < max_digit; i++)
+			count[i] = count[i] + count[i - 1];
 
-        /*Get the count of times that integers appear in array - frequency*/
-        for (i = 0; i < size; i++)
-            ++count[(array[i] / pos) % 10];
+		/*Build Output array*/
+		for (i = size - 1; i <= size; i--)
+			output_array[--count[(array[i] / pos) % 10]] = array[i];
 
-        /*Perform a cumulative addition on the integers from left to right*/
-        for (i = 1; i < max_digit; i++)
-            count[i] = count[i] + count[i - 1];
+		print_array(output_array, size);
 
-        /*Build Output array*/
-        for (i = size - 1; i <= size; i--)
-            output_array[--count[(array[i] / pos) % 10]] = array[i];
-
-        print_array(output_array, size);
-
-        /*Copy sorted list from Output array to initial array*/
-        for (i = 0; i < size; i++)
-            array[i] = output_array[i];
-
-        /*free(output_array);
-        free(count);*/
-    }
-    free(output_array);
-    free(count);
+		/*Copy sorted list from Output array to initial array*/
+		for (i = 0; i < size; i++)
+			array[i] = output_array[i];
+	}
+	free(output_array);
+	free(count);
 }
